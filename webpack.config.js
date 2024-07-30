55df777cffbabdc48465fff7796d8f38784a3e72
MellMoy <swaggadanil@mail.ru>
2024-07-30 21:12:35 +0700

    webpack homework

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    stats :{
+        children : true,
+        //     macModules : 0
+            },
+    mode : 'development',
+    entry: './src/new.ts',
+    output: {
+        filename: 'main.js',
+        
+        
+    },
+    
+// настройка dev-server
+    devServer: {
+        // contentBase: './dist',
+        static : './dist',
+        port : 3000,
+        hot : true, // полезная для работы с реактом
+        open : true,
+        
+        
+    },
+
+    plugins: [ 
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'development', 
+            template: './src/index.pug',
+            filename: 'index.html'
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerWebpackPlugin(),
+            new TerserWebpackPlugin(),
+        ]
+    },    
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            esModule: true,
+                        }
+                    },
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.pug$/,
+                use: [
+                    {
+                        loader : 'pug-loader',
+                        options: { 
+                            pretty: true,
+
+                        },
+
+                    },
+                ], 
+                    
+                
+            },
+            {
+                test: /\.ts$/,
+                use: 'ts-loader'
+            }
+        ]
+    }
+};
